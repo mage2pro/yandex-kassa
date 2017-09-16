ca73f546118c0e473e015b33e6f354e9ef89516e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-17 02:31:10 +0300

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -1,6 +1,6 @@
 <?php
 namespace Dfe\YandexKassa;
-use Df\PaypalClone\Source\OptionsLocation as OL;
+use Df\Payment\Settings\Options as O;
 // 2017-09-17
 final class ConfigProvider extends \Df\Payment\ConfigProvider {
 	/**
@@ -10,10 +10,15 @@ final class ConfigProvider extends \Df\Payment\ConfigProvider {
 	 * @used-by \Df\Payment\ConfigProvider::getConfig()
 	 * @return array(string => mixed)
 	 */
-	protected function config() {/** @var Settings $s */ $s = $this->s(); return [
-		// 2017-09-17
-		// @used-by Df_Payments/withOptions::options()
-		// https://github.com/mage2pro/core/blob/2.0.36/Payment/view/frontend/web/withOptions.js?ts=4#L55
-		'options' => $s->options()->o()
-	] + parent::config();}
+	protected function config() {
+		$s = $this->s(); /** @var Settings $s */
+		$o = $s->options();	/** @var O $o */
+		return [
+			'needShowOptions' => $o->needShow()
+			// 2017-09-17
+			// @used-by Df_Payments/withOptions::options()
+			// https://github.com/mage2pro/core/blob/2.0.36/Payment/view/frontend/web/withOptions.js?ts=4#L55
+			,'options' => $o->o(true)
+		] + parent::config();
+	}
 }
