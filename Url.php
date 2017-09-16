241918706a45c098c67c150cdc46db5629e67738
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-16 11:52:48 +0300

    no message

--- Url.php
+++ Url.php
@@ -12,7 +12,8 @@ final class Url extends \Df\Payment\Url {
 	 * 2.1.1. The production mode:
 	 * «When the user clicks the `Pay` button,
 	 * a set of payment parameters is sent to Yandex.Checkout at https://money.yandex.ru/eshop.xml»
-	 * «Payment solution protocol for merchants» → «Payment form» → «Form for HTTP notifications»:
+	 * «Payment solution protocol for merchants» → «Payment form» → «Form for HTTP notifications»
+	 * → «Form parameters»:
 	 * https://tech.yandex.com/money/doc/payment-solution/payment-form/payment-form-docpage
 	 * 2.1.2. The test mode:
 	 * «Address for sending parameters in testing mode: https://demomoney.yandex.ru/eshop.xml»
@@ -21,7 +22,8 @@ final class Url extends \Df\Payment\Url {
 	 * 2.2 In Russian:
 	 * 2.2.1. Промышленный режим:
 	 * «Адрес для отправки формы: https://money.yandex.ru/eshop.xml»
-	 * «Протокол приема платежей для магазинов» → «Платежная форма» → «Форма для HTTP-уведомлений»:
+	 * «Протокол приема платежей для магазинов» → «Платежная форма» → «Форма для HTTP-уведомлений»
+	 * → «Параметры формы»:
 	 * https://tech.yandex.ru/money/doc/payment-solution/payment-form/payment-form-http-docpage
 	 * 2.2.2. Тестовый режим:
 	 * «Адрес для отправки параметров в тестовом режиме: https://demomoney.yandex.ru/eshop.xml»
