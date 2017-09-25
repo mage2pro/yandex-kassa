dc93e79b723f67f419ca16ea202c77428537cc8a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-25 14:42:44 +0300

    0.2.4

--- Settings.php
+++ Settings.php
@@ -15,7 +15,7 @@ final class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2017-09-16 «scid».
 	 * [Yandex.Kassa] Where to find my «scid»? https://mage2.pro/t/4520
-	 * @used-by @used-by \Dfe\YandexKassa\Charge::pCharge()
+	 * @used-by \Dfe\YandexKassa\Charge::pCharge()
 	 * @return int
 	 */
 	function scid() {return $this->i();}
--- Signer.php
+++ Signer.php
@@ -1,27 +1,45 @@
 <?php
 namespace Dfe\YandexKassa;
 /**
- * 2017-04-13
- * @see \Dfe\YandexKassa\Signer\Request
- * @see \Dfe\YandexKassa\Signer\Response
+ * 2017-09-25
+ * Note 1.
+ * «HTTP notifications about payments»
+ * MD5 hashing is applied to a text
+ * that is formed as a sequence of values for a set of request parameters separated by semicolons (;).
+ * The hash result is converted to uppercase.
+ * Note.
+ * Make sure the `customerNumber` value doesn't have any spaces at the beginning or at the end.
+ * Order of parameters:
+ * action;orderSumAmount;orderSumCurrencyPaycash;orderSumBankPaycash;shopId;invoiceId;customerNumber;shopPassword
+ * https://tech.yandex.com/money/doc/payment-solution/payment-notifications/payment-notifications-http-docpage
+ *
+ * 1.1. In Russian:
+ * «HTTP-уведомления о переводах»
+ * «MD5-хэширование применяется к тексту,
+ * который формируется как последовательность значений ряда параметров запроса,
+ * разделенных символом «точка с запятой» (;).
+ * Результат хэширования приводится к верхнему регистру.
+ * Примечание.
+ * Проверьте, что в значении `customerNumber` нет пробелов в конце или в начале.
+ * Порядок следования параметров:
+ * action;orderSumAmount;orderSumCurrencyPaycash;orderSumBankPaycash;shopId;invoiceId;customerNumber;shopPassword»
+ * https://tech.yandex.ru/money/doc/payment-solution/payment-notifications/payment-notifications-http-docpage
+ * Note 2.
+ * The Yandex.Kassa charge requests do not use a signature:
+ * @see \Dfe\YandexKassa\Charge::k_Signature()
+ * https://tech.yandex.com/money/doc/payment-solution/payment-form/payment-form-http-docpage
  * @method Settings s()
  */
-abstract class Signer extends \Df\PaypalClone\Signer {
+final class Signer extends \Df\PaypalClone\Signer {
 	/**
-	 * 2017-04-13
-	 * @used-by sign()
-	 * @see \Dfe\YandexKassa\Signer\Request::values()
-	 * @see \Dfe\YandexKassa\Signer\Response::values()
-	 * @return string[]
-	 */
-	abstract protected function values();
-
-	/**
-	 * 2017-04-13
+	 * 2017-09-25
 	 * @override
 	 * @see \Df\PaypalClone\Signer::sign()
 	 * @used-by \Df\PaypalClone\Signer::_sign()
 	 * @return string
 	 */
-	final protected function sign() {return implode($this->values());}
+	protected function sign() {$s = $this->s(); return md5(implode(';', $this->v([
+		'action', 'orderSumAmount', 'orderSumCurrencyPaycash', 'orderSumBankPaycash', $s->merchantID()
+		,'invoiceId', 'customerNumber', $s->privateKey()
+	])));}
 }
