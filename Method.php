0f5f4431a6b7c95cbdf17a8910195059efa42d2c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-29 16:27:50 +0300

    0.2.7

--- Method.php
+++ Method.php
@@ -18,7 +18,7 @@ final class Method extends \Df\PaypalClone\Method {
 	 * @param float|int $a
 	 * @return string
 	 */
-	function amountFormat($a) {return df_f2($a);}
+	function amountFormat($a) {return dff_2($a);}
 
 	/**
 	 * 2017-09-24
