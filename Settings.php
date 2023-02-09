1a25b3aa3b9a1ac8eaf049113a5a329010675a24
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-09 22:18:22 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings.php
+++ Settings.php
@@ -15,7 +15,6 @@ final class Settings extends \Df\Payment\Settings {
 	 * 2017-09-16 «scid».
 	 * [Yandex.Kassa] Where to find my «scid»? https://mage2.pro/t/4520
 	 * @used-by \Dfe\YandexKassa\Charge::pCharge()
-	 * @return int
 	 */
-	function scid() {return $this->i();}
+	function scid():int {return $this->i();}
 }
